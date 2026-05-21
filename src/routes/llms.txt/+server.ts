886c1afd6170fa3c555cb2257f90916b5d9cf8dd
janvier-s <janvier.sabates@gmail.com>
2026-05-21 22:35:06 +0200

    feat: llms.txt corpus summary

---
+++ src/routes/llms.txt/+server.ts
@@ -0,0 +1,28 @@
+import { topics, authors, works, quotes } from '$lib/data';
+
+export const prerender = true;
+
+export function GET() {
+	const body = [
+		"# Pères de l'Église",
+		'Anthologie patristique française organisée par sujets.',
+		'',
+		'## Stats',
+		`- Auteurs: ${authors.length}`,
+		`- Œuvres: ${works.length}`,
+		`- Sujets: ${topics.length}`,
+		`- Citations: ${quotes.length}`,
+		'',
+		'## Routes',
+		'- /sujets',
+		'- /sujets/[slug]',
+		'- /peres',
+		'- /peres/[slug]',
+		'- /oeuvres',
+		'- /oeuvres/[slug]',
+		'- /citation/[id]',
+		'- /recherche'
+	].join('\n');
+
+	return new Response(body, { headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
+}
