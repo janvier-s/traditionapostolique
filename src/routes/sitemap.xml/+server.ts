c607289b6bc71a9f9e93455d4452df845d4618c1
janvier-s <janvier.sabates@gmail.com>
2026-05-21 22:34:24 +0200

    feat: sitemap.xml endpoint listing all routes

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,30 @@
+import { topics, authors, works, quotes } from '$lib/data';
+
+const STATIC_ROUTES = [
+	'/',
+	'/sujets',
+	'/peres',
+	'/oeuvres',
+	'/recherche',
+	'/a-propos',
+	'/mentions-legales'
+];
+
+export const prerender = true;
+
+export function GET() {
+	const urls = [
+		...STATIC_ROUTES,
+		...topics.map((t) => `/sujets/${t.slug}`),
+		...authors.map((a) => `/peres/${a.slug}`),
+		...works.map((w) => `/oeuvres/${w.slug}`),
+		...quotes.map((q) => `/citation/${q.id}`)
+	];
+
+	const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((u) => `  <url><loc>https://pereseglise.pages.dev${u}</loc></url>`).join('\n')}
+</urlset>`;
+
+	return new Response(body, { headers: { 'Content-Type': 'application/xml' } });
+}
